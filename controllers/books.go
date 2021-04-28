27b02a958fe5b36b3a802963e974283e973cd11d
Sheikh2Imran <imranjustcse@gmail.com>
2021-04-28 10:43:12 +0600

    book update and delete api is added

--- controllers/books.go
+++ controllers/books.go
@@ -18,7 +18,7 @@ type CreateBookInput struct {
 	Author string `json:"author" binding:"required"`
 }
 
-func CreateBook(c *gin.Context)  {
+func CreateBook(c *gin.Context) {
 	// validate input
 	var input CreateBookInput
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -42,4 +42,39 @@ func FindBook(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func UpdateBook (c *gin.Context) {
+	// grab a single book and make sure it exists
+	var book models.Book
+	if err := models.DB.Where("id=?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
+		return
+	}
+
+	// validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id=?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"data": err.Error()})
+		return
+	}
+
+	models.DB.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
